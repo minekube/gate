b116588e4ca4225935eb7cf3f1df931ed17572d4
Robin <robin.braemer@web.de>
2021-06-10 12:10:41 +0200

    update /server command

---
+++ pkg/edition/java/proxy/builtin_cmd_server.go
@@ -0,0 +1,144 @@
+package proxy
+
+import (
+	"context"
+	"fmt"
+	"go.minekube.com/brigodier"
+	. "go.minekube.com/common/minecraft/color"
+	. "go.minekube.com/common/minecraft/component"
+	"go.minekube.com/gate/pkg/command"
+	"go.minekube.com/gate/pkg/internal/suggest"
+	"sort"
+	"strings"
+	"time"
+)
+
+const serverCmdPermission = "gate.command.server"
+
+func newServerCmd(proxy *Proxy) brigodier.LiteralNodeBuilder {
+	return brigodier.Literal("server").
+		Requires(hasCmdPerm(proxy, serverCmdPermission)).
+		// List registered server.
+		Executes(command.Command(func(c *command.Context) error {
+			return c.SendMessage(serversInfo(proxy, c.Source))
+		})).
+		// Switch server
+		Then(brigodier.Argument("name", brigodier.StringWord).
+			Suggests(serverSuggestionProvider(proxy)).
+			Executes(command.Command(func(c *command.Context) error {
+				player, ok := c.Source.(Player)
+				if !ok {
+					return c.Source.SendMessage(&Text{S: Style{Color: Red},
+						Content: "Only players can connect to a server!"})
+				}
+
+				name := c.String("name")
+				rs := proxy.Server(name)
+				if rs == nil {
+					return c.Source.SendMessage(&Text{S: Style{Color: Red},
+						Content: fmt.Sprintf("Server %q doesn't exist.", name)})
+				}
+
+				ctx, cancel := context.WithTimeout(context.Background(),
+					time.Millisecond*time.Duration(proxy.config.ConnectionTimeout))
+				defer cancel()
+				player.CreateConnectionRequest(rs).ConnectWithIndication(ctx)
+				return nil
+			})),
+		)
+}
+
+const maxServersToList = 50
+
+func serversInfo(proxy *Proxy, s command.Source) (c Component) {
+	info := &Text{Content: "\n", S: Style{Color: Yellow}}
+	c = info
+	add := func(c Component) { info.Extra = append(info.Extra, c) }
+
+	// Show current server
+	var current string
+	if p, ok := s.(Player); ok {
+		curr := p.CurrentServer()
+		if curr != nil {
+			current = curr.Server().ServerInfo().Name()
+			add(&Text{Content: fmt.Sprintf("You are currently connected to %q.\n", current)})
+		}
+	}
+
+	servers := proxy.Servers()
+	sortServers(servers)
+
+	// Assemble the list of servers as components
+	list := &Text{S: Style{Color: Gray}}
+	add(&Text{
+		Content: fmt.Sprintf("Available servers (%d):\n\n", len(servers)),
+		Extra:   []Component{list},
+	})
+	split := &Text{Content: ", "}
+	for i, server := range servers {
+		if i+1 == maxServersToList {
+			list.Extra = append(list.Extra, &Text{
+				Content: fmt.Sprintf(
+					"\n\nand %d more servers...", len(servers)-i),
+				S: Style{HoverEvent: ShowText(&Text{Content: "Tab-complete to search more servers."})},
+			})
+			break
+		}
+		if i != 0 {
+			list.Extra = append(list.Extra, split)
+		}
+		list.Extra = append(list.Extra, formatServerComponent(current, server))
+	}
+	return
+}
+
+func formatServerComponent(currentPlayerServer string, s RegisteredServer) Component {
+	name := s.ServerInfo().Name()
+	c := &Text{Content: name}
+	size := s.Players().Len()
+	playersText := fmt.Sprintf("%d %s online", size, plural("player", size))
+	cmd := fmt.Sprintf("/server %s", name)
+	if currentPlayerServer == name {
+		c.S = Style{Color: Red,
+			HoverEvent: ShowText(&Text{Content: fmt.Sprintf(
+				"Currently connected to this server\n%s", playersText)}),
+			ClickEvent: SuggestCommand(cmd),
+		}
+	} else {
+		c.S = Style{Color: Gray,
+			HoverEvent: ShowText(&Text{Content: fmt.Sprintf(
+				"Click to connect to this server\n%s", playersText)}),
+			ClickEvent: RunCommand(cmd),
+		}
+	}
+	return c
+}
+
+func plural(s string, i int) string {
+	if i == 0 || i > 1 || i < -1 {
+		return s + "s"
+	}
+	return s
+}
+
+// sort servers by name
+func sortServers(s []RegisteredServer) {
+	sort.Slice(s, func(i, j int) bool {
+		return s[i].ServerInfo().Name() < s[j].ServerInfo().Name()
+	})
+}
+
+func serverSuggestionProvider(p *Proxy) brigodier.SuggestionProvider {
+	return suggest.ProviderFunc(func(
+		_ *brigodier.CommandContext,
+		b *brigodier.SuggestionsBuilder,
+	) *brigodier.Suggestions {
+		servers := p.Servers()
+		candidates := make([]string, len(servers))
+		for i, s := range servers {
+			candidates[i] = s.ServerInfo().Name()
+		}
+		given := b.Input[strings.Index(b.Input, " ")+1:]
+		return suggest.Build(b, given, candidates)
+	})
+}
--- pkg/edition/java/proxy/builtin_commands.go
+++ pkg/edition/java/proxy/builtin_commands.go
@@ -1,15 +1,8 @@
 package proxy
 
 import (
-	"context"
-	"fmt"
 	"go.minekube.com/brigodier"
-	. "go.minekube.com/common/minecraft/color"
-	. "go.minekube.com/common/minecraft/component"
 	"go.minekube.com/gate/pkg/command"
-	"go.minekube.com/gate/pkg/internal/suggest"
-	"strings"
-	"time"
 )
 
 func (p *Proxy) registerBuiltinCommands() {
@@ -19,75 +12,5 @@ func (p *Proxy) registerBuiltinCommands() {
 func hasCmdPerm(proxy *Proxy, perm string) brigodier.RequireFn {
 	return command.Requires(func(c *command.RequiresContext) bool {
 		return !proxy.config.RequireBuiltinCommandPermissions || c.Source.HasPermission(perm)
-	})
-}
-
-const serverCmdPermission = "gate.command.server"
-
-func newServerCmd(proxy *Proxy) brigodier.LiteralNodeBuilder {
-	return brigodier.Literal("server").
-		Requires(hasCmdPerm(proxy, serverCmdPermission)).
-		// List registered server.
-		Executes(command.Command(func(c *command.Context) error {
-			const maxEntries = 50
-			var servers []Component
-			proxyServers := proxy.Servers()
-			for i, s := range proxyServers {
-				if i+1 == maxEntries {
-					servers = append(servers, &Text{
-						Content: fmt.Sprintf("and %d more...", len(proxyServers)-i+1),
-					})
-					break
-				}
-				servers = append(servers, &Text{
-					Content: fmt.Sprintf("  %s - %s (%d players)\n",
-						s.ServerInfo().Name(), s.ServerInfo().Addr(), s.Players().Len()),
-					S: Style{ClickEvent: RunCommand(fmt.Sprintf("/server %s", s.ServerInfo().Name()))},
-				})
-			}
-			return c.Source.SendMessage(&Text{
-				Content: fmt.Sprintf("\nServers (%d):\n", len(proxyServers)),
-				S:       Style{Color: Green},
-				Extra: []Component{&Text{
-					S: Style{
-						Color:      Yellow,
-						HoverEvent: ShowText(&Text{Content: "Click to connect!", S: Style{Color: Green}}),
-					},
-					Extra: servers,
-				}},
-			})
-		})).
-		// Switch server
-		Then(brigodier.Argument("name", brigodier.StringWord).
-			Suggests(serverSuggestionProvider(proxy)).
-			Executes(command.Command(func(c *command.Context) error {
-				player, ok := c.Source.(Player)
-				if !ok {
-					return c.Source.SendMessage(&Text{Content: "Only players can connect to a server!", S: Style{Color: Red}})
-				}
-
-				name := c.String("name")
-				rs := proxy.Server(name)
-				if rs == nil {
-					return c.Source.SendMessage(&Text{Content: fmt.Sprintf("Server %q not registered", name), S: Style{Color: Red}})
-				}
-
-				ctx, cancel := context.WithTimeout(context.Background(), time.Millisecond*time.Duration(proxy.config.ConnectionTimeout))
-				defer cancel()
-				player.CreateConnectionRequest(rs).ConnectWithIndication(ctx)
-				return nil
-			})),
-		)
-}
-
-func serverSuggestionProvider(p *Proxy) brigodier.SuggestionProvider {
-	return suggest.ProviderFunc(func(_ *brigodier.CommandContext, b *brigodier.SuggestionsBuilder) *brigodier.Suggestions {
-		servers := p.Servers()
-		candidates := make([]string, len(servers))
-		for i, s := range servers {
-			candidates[i] = s.ServerInfo().Name()
-		}
-		given := b.Input[strings.Index(b.Input, " ")+1:]
-		return suggest.Build(b, given, candidates)
 	})
 }
