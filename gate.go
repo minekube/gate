e41e41a33cc7a1e8c1d09a2b28412450b5f660ff
Daniel Gran <contact@grandaniel.com>
2023-06-12 18:53:28 +0200

    Removed Dead Code (#212)

--- gate.go
+++ gate.go
@@ -5,18 +5,5 @@ import (
 )
 
 func main() {
-
-	//if err := agent.Listen(agent.ServeOptions{
-	//	ShutdownCleanup: true,
-	//}); err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//if profiling {
-	//	go func() {
-	//		http.ListenAndServe("localhost:8080", pprof.Handler("heap"))
-	//	}()
-	//}
-
 	gate.Execute()
 }
