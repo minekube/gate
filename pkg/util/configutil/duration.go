b034584eeac22a2c5296f17f88681f0d2d99fbbe
Robin <robin.braemer@web.de>
2023-07-23 19:24:22 +0200

    fix: Implement YAML en-/decoder (fixes #223)

--- pkg/util/configutil/duration.go
+++ pkg/util/configutil/duration.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
 // Duration is a configuration duration.
@@ -13,28 +15,57 @@ import (
 //   - int64 and float64 are interpreted as seconds.
 type Duration time.Duration
 
+// Make sure Duration implements the interfaces at compile time.
+var (
+	_ yaml.Marshaler   = (*Duration)(nil)
+	_ yaml.Unmarshaler = (*Duration)(nil)
+
+	_ json.Marshaler   = (*Duration)(nil)
+	_ json.Unmarshaler = (*Duration)(nil)
+)
+
 func (d *Duration) MarshalJSON() ([]byte, error) {
 	return json.Marshal(time.Duration(*d).String())
 }
-
 func (d *Duration) UnmarshalJSON(data []byte) error {
 	var a any
 	if err := json.Unmarshal(data, &a); err != nil {
 		return err
 	}
+	dur, err := decode(a)
+	if err != nil {
+		return err
+	}
+	*d = Duration(dur)
+	return nil
+}
+
+func (d *Duration) MarshalYAML() (interface{}, error) {
+	return time.Duration(*d).String(), nil
+}
+func (d *Duration) UnmarshalYAML(value *yaml.Node) error {
+	var a any
+	err := value.Decode(&a)
+	if err != nil {
+		return err
+	}
+	dur, err := decode(a)
+	if err != nil {
+		return err
+	}
+	*d = Duration(dur)
+	return nil
+}
+
+func decode(a any) (time.Duration, error) {
 	switch v := a.(type) {
 	case string:
-		dur, err := time.ParseDuration(v)
-		if err != nil {
-			return err
-		}
-		*d = Duration(dur)
+		return time.ParseDuration(v)
 	case float64:
-		*d = Duration(time.Duration(v) * time.Second)
+		return time.Duration(v) * time.Second, nil
 	case int64:
-		*d = Duration(time.Duration(v) * time.Second)
+		return time.Duration(v) * time.Second, nil
 	default:
-		return fmt.Errorf("invalid duration type %T: %v", v, v)
+		return 0, fmt.Errorf("invalid duration type %T: %v", v, v)
 	}
-	return nil
 }
