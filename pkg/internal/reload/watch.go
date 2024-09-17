fdb066d94eead3c3a0ef2760d02dc42ee89d51a5
oｍ(o・ω・o)ｍ <61079068+ours1505@users.noreply.github.com>
2024-09-17 19:52:06 +0800

    fix auto reload #359

--- pkg/internal/reload/watch.go
+++ pkg/internal/reload/watch.go
@@ -2,20 +2,24 @@ package reload
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/knadh/koanf/providers/file"
 )
 
-// Watch watches the given path for changes and calls the given callback.
+var mu sync.Mutex
+var debounceTimer *time.Timer
+
+const debounceDuration = 100 * time.Millisecond
+
 func Watch(ctx context.Context, path string, cb func() error) error {
 	if ctx.Err() != nil {
 		return nil
 	}
 	log := logr.FromContextOrDiscard(ctx).WithValues("path", path)
 	return file.Provider(path).Watch(func(_ any, err error) {
-		// TODO there is no way to stop watching, watchers could pile up
 		if ctx.Err() != nil {
 			return
 		}
@@ -23,12 +27,23 @@ func Watch(ctx context.Context, path string, cb func() error) error {
 			log.Info("failed watching config", "error", err)
 			return
 		}
-		log.Info("auto-reloading config")
-		start := time.Now()
-		if err := cb(); err != nil {
-			log.Info("failed to reload config", "error", err)
-			return
+
+		mu.Lock()
+		if debounceTimer != nil {
+			debounceTimer.Stop()
 		}
-		log.Info("reloaded config successfully", "duration", time.Since(start).Round(time.Millisecond).String())
+		debounceTimer = time.AfterFunc(debounceDuration, func() {
+			mu.Lock()
+			defer mu.Unlock()
+
+			log.Info("auto-reloading config")
+			start := time.Now()
+			if err := cb(); err != nil {
+				log.Info("failed to reload config", "error", err)
+				return
+			}
+			log.Info("reloaded config successfully", "duration", time.Since(start).Round(time.Millisecond).String())
+		})
+		mu.Unlock()
 	})
 }
