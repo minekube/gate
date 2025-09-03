04f65b56a3bc5c475dbfa0e56ae08a0ad0b8a860
robinbraemer <robin.braemer@web.de>
2025-09-03 13:41:22 +0200

    feat: Move DeviceOS handling to a dedicated file
    - Created a new file for DeviceOS struct and its associated constants, enhancing code organization and clarity.
    - Implemented methods for identifying device types (console, mobile, desktop, etc.) based on DeviceOS.
    - Removed DeviceOS definitions from BedrockData to streamline the structure and improve maintainability.

--- pkg/edition/bedrock/geyser/floodgate/floodgate.go
+++ pkg/edition/bedrock/geyser/floodgate/floodgate.go
@@ -34,68 +34,6 @@ type BedrockData struct {
 	VerifyCode   string   // Verification code for linking
 }
 
-// DeviceOS represents the operating system of a device.
-type DeviceOS struct {
-	ID   int
-	Name string
-}
-
-// String implements fmt.Stringer.
-func (d DeviceOS) String() string {
-	return d.Name
-}
-
-// DeviceOS constants
-// See https://github.com/GeyserMC/Geyser/blob/master/common/src/main/java/org/geysermc/floodgate/util/DeviceOs.java#L35
-var (
-	DeviceOSUnknown      = DeviceOS{ID: 0, Name: "Unknown"}
-	DeviceOSAndroid      = DeviceOS{ID: 1, Name: "Android"}
-	DeviceOSIOS          = DeviceOS{ID: 2, Name: "iOS"}
-	DeviceOSMacOS        = DeviceOS{ID: 3, Name: "macOS"}
-	DeviceOSAmazon       = DeviceOS{ID: 4, Name: "Amazon"}
-	DeviceOSGearVR       = DeviceOS{ID: 5, Name: "Gear VR"}
-	DeviceOSHololens     = DeviceOS{ID: 6, Name: "Hololens"} // Deprecated
-	DeviceOSWindowsUWP   = DeviceOS{ID: 7, Name: "Windows"}
-	DeviceOSWindowsX86   = DeviceOS{ID: 8, Name: "Windows x86"}
-	DeviceOSDedicated    = DeviceOS{ID: 9, Name: "Dedicated"}
-	DeviceOSAppleTV      = DeviceOS{ID: 10, Name: "Apple TV"}    // Deprecated
-	DeviceOSPlayStation  = DeviceOS{ID: 11, Name: "PlayStation"} // All PlayStation platforms
-	DeviceOSSwitch       = DeviceOS{ID: 12, Name: "Switch"}
-	DeviceOSXbox         = DeviceOS{ID: 13, Name: "Xbox"}
-	DeviceOSWindowsPhone = DeviceOS{ID: 14, Name: "Windows Phone"} // Deprecated
-	DeviceOSLinux        = DeviceOS{ID: 15, Name: "Linux"}
-)
-
-// DeviceOSes is a list of all DeviceOSes.
-var DeviceOSes = []DeviceOS{
-	DeviceOSUnknown,
-	DeviceOSAndroid,
-	DeviceOSIOS,
-	DeviceOSMacOS,
-	DeviceOSAmazon,
-	DeviceOSGearVR,
-	DeviceOSHololens,
-	DeviceOSWindowsUWP,
-	DeviceOSWindowsX86,
-	DeviceOSDedicated,
-	DeviceOSAppleTV,
-	DeviceOSPlayStation,
-	DeviceOSSwitch,
-	DeviceOSXbox,
-	DeviceOSWindowsPhone,
-	DeviceOSLinux,
-}
-
-// DeviceOSFromID returns the DeviceOS with the given ID.
-func DeviceOSFromID(id int) DeviceOS {
-	for _, os := range DeviceOSes {
-		if os.ID == id {
-			return os
-		}
-	}
-	return DeviceOSUnknown
-}
-
 // NewFloodgate creates a new Floodgate instance with the given encryption key.
 func NewFloodgate(key []byte) (*Floodgate, error) {
 	cipher, err := NewAesCipher(key)
