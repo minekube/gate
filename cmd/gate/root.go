080575f361a1c7a90f64f556e0c64df057b75077
NixNux123 <nixnux123@fynncraft.de>
2025-10-08 20:25:57 +0000

    feat: Support overriding forwarding secrets with environment variables (#575)
    * feat: Support overriding forwarding secrets with environment variables
    
    Binds the `velocitySecret` and `bungeeGuardSecret` configuration fields to
    dedicated environment variables for greater operational flexibility and security. Environment variables are always overwriting the config values.
    
    The environment variables are:
    - `GATE_VELOCITY_SECRET`
    - `GATE_BUNGEEGUARD_SECRET`
    
    * feat: Check error returns for v.BindEnv

--- cmd/gate/root.go
+++ cmd/gate/root.go
@@ -158,6 +158,16 @@ func initViper(c *cli.Context, configFile string) (*viper.Viper, error) {
 	v.SetEnvPrefix("GATE")
 	v.AutomaticEnv() // read in environment variables that match
 	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	
+	// Bind custom environment variables for forwarding secrets
+	if err := v.BindEnv("velocitySecret", "GATE_VELOCITY_SECRET"); err != nil {
+	    log.Fatalf("Error binding environment variable 'GATE_VELOCITY_SECRET': %v", err)
+	}
+
+	if err := v.BindEnv("bungeeGuardSecret", "GATE_BUNGEEGUARD_SECRET"); err != nil {
+	    log.Fatalf("Error binding environment variable 'GATE_BUNGEEGUARD_SECRET': %v", err)
+	}
+	
 	return v, nil
 }
 
